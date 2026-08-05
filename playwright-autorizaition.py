d2c022d2ea4b038dc24bf77d6ea0db34b5825b79
Nik89143 <zubov.nikita.2012@mail.ru>
2026-08-05 12:21:07 +0300

    Интеграция pytest  с playwright

--- playwright-autorizaition.py
+++ playwright-autorizaition.py
@@ -1,22 +1,23 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
-    user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    user_name_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill('password')
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    login_button = page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
     page.wait_for_timeout(5000)
 
